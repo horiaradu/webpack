f842c8d0829484f07426f0b66a86a6312d88d739
iuvo <alpha@iuvo.ai>
2017-10-10 09:29:06 +0000

    replace with better commit message

--- test/watchCases/plugins/watch-ignore-plugin/0/index.js
+++ test/watchCases/plugins/watch-ignore-plugin/0/index.js
@@ -2,7 +2,7 @@ import value from "./file"
 import a from "./a"
 const req = require.context("./foo", false, /^.*\.js$/);
 it("should ignore change to file and directory", function() {
-	a.should.be.eql(+WATCH_STEP);
+	a.should.be.eql(Number(WATCH_STEP));
 	req.keys().should.be.deepEqual(["./0.js"])
 	value.should.be.eql(1);
 });
