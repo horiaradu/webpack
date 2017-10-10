edb085fb6b33f12de58d0282557a1f3da78a5523
iuvo <alpha@iuvo.ai>
2017-10-10 11:21:47 +0000

    replace with better commit message

--- test/configCases/async-commons-chunk/nested/index.js
+++ test/configCases/async-commons-chunk/nested/index.js
@@ -5,16 +5,16 @@ it("should load nested commons chunk", function(done) {
 		require.ensure(["./b", "./c"], function(require) {
 			require("./b").should.be.eql("b");
 			require("./c").should.be.eql("c");
-			if(++counter == 3) done();
+			if (++counter === 3) done();
 		});
 		require.ensure(["./b", "./d"], function(require) {
 			require("./b").should.be.eql("b");
 			require("./d").should.be.eql("d");
-			if(++counter == 3) done();
+			if (++counter == 3) done();
 		});
 		require.ensure(["./b"], function(require) {
 			require("./b").should.be.eql("b");
-			if(++counter == 3) done();
+			if (++counter == 3) done();
 		});
 	});
 });
