c792a53172a68b5b1874c698dd04946315314126
iuvo <alpha@iuvo.ai>
2017-10-10 11:09:57 +0000

    replace with better commit message

--- test/browsertest/build.js
+++ test/browsertest/build.js
@@ -27,13 +27,13 @@ console.log("compile scripts...");
 
 var extraArgsNoWatch = extraArgs.slice(0);
 var watchIndex = extraArgsNoWatch.indexOf("--watch");
-if(watchIndex != -1) extraArgsNoWatch.splice(watchIndex, 1);
+if (watchIndex !== -1) extraArgsNoWatch.splice(watchIndex, 1);
 // node ../../bin/webpack --output-pathinfo --color --optimize-max-chunks 1 --output-library library1 node_modules/library1 js/library1
 var library1 = cp.spawn("node", join(["../../bin/webpack.js", "--output-pathinfo", "--color", "--optimize-max-chunks", "1", "--output-library", "library1",
 										"node_modules/library1", "js/library1.js"], extraArgsNoWatch));
 bindOutput(library1);
 library1.on("exit", function(code) {
-	if(code === 0) {
+	if (code === 0) {
 		// node ../../bin/webpack --output-pathinfo --color --resolve-alias vm=vm-browserify --output-public-path js/ --output-chunk-filename [name].web.js --module-bind css=style!css --module-bind less=style-loader!css-loader!less-loader --module-bind coffee --module-bind jade --prefetch ./lib/stylesheet.less ./lib/index "js/web.js?h=[hash]"
 		var main = cp.spawn("node", join(["../../bin/webpack.js", "--output-pathinfo", "--color", "--resolve-alias", "vm=vm-browserify",
 											"--output-public-path", "js/", "--output-chunk-filename", "[name].web.js",
